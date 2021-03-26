00a6435a6bc789a7eadddc2dd4d860677a3a6812
Sounkoun Soumare <vsoumare@yahoo.com>
2021-03-26 05:43:37 -0400

    change from other repo, repo is updated

--- firstSpringAndAngular/src/main/java/com/saraya/firstSpringAndAngular/hello/HelloWorldController.java
+++ firstSpringAndAngular/src/main/java/com/saraya/firstSpringAndAngular/hello/HelloWorldController.java
@@ -21,12 +21,12 @@ public class HelloWorldController {
 	@GetMapping("/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		//throw new RuntimeException("U get some error");
-		return new HelloWorldBean("Hello World from the Bean how are you with github");
+		return new HelloWorldBean("Hello World from the Bean to the bean how are you with github");
 	}
 	
 	@GetMapping("/hello-world-bean/{name}")
 	public HelloWorldBean helloWorldBeanWithPathVariable(@PathVariable String name) {
 		//throw new RuntimeException("U get some error");
-		return new HelloWorldBean(String.format("Hello World from %s", name));
+		return new HelloWorldBean(String.format("Hello we love coding World from %s", name));
 	}
 }
